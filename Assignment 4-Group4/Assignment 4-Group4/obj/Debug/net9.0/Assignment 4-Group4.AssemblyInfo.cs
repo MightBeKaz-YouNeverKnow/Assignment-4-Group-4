6fa1504e42f703e05b127803d1c6fb252a2e1445
MightBeKaz-YouNeverKnow <000972147@mohawkcollege.ca>
2025-11-13 13:41:37 -0500

    A

--- Assignment 4-Group4/Assignment 4-Group4/obj/Debug/net9.0/Assignment 4-Group4.AssemblyInfo.cs
+++ Assignment 4-Group4/Assignment 4-Group4/obj/Debug/net9.0/Assignment 4-Group4.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assignment 4-Group4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5d39404ddfe06eb6b2e9915e95ab54c54412a6f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b649f7da7d652576627bd9ea6a75ebecf948d6be")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assignment 4-Group4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assignment 4-Group4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
