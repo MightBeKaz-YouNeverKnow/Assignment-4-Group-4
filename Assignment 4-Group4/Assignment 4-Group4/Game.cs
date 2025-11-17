59eff3d042d6c81a91f160fc642650ca12ec0a83
JamesonPhong <000882371@mohawkcollege.ca>
2025-11-17 13:06:12 -0500

    Remove unnecessary code comments

--- Assignment 4-Group4/Assignment 4-Group4/Game.cs
+++ Assignment 4-Group4/Assignment 4-Group4/Game.cs
@@ -1,18 +1,11 @@
-﻿// Include the namespaces (code libraries) you need below.
-using Raylib_cs;
+﻿using Raylib_cs;
 using System;
 using System.Numerics;
 
-// The namespace your code is in.
 namespace MohawkGame2D
 {
-    /// <summary>
-    ///     Your game code goes inside this class!
-    /// </summary>
     public class Game
     {
-        // Place your variables here:
-
         // Player 1 XY position
         float player1x = 0.0f;
         float player1y = 0.0f;
@@ -24,19 +17,11 @@ namespace MohawkGame2D
         // Player Speed
         float player1Speed = 4.0f;
         float player2Speed = 4.0f;
-
-        /// <summary>
-        ///     Setup runs once before the game loop begins.
-        /// </summary>
         public void Setup()
         {
             Window.SetTitle("Assignment 4 - Group 4");
             Window.SetSize(1200, 600);
         }
-
-        /// <summary>
-        ///     Update runs every frame.
-        /// </summary>
         public void Update()
         {
             Window.ClearBackground(Color.OffWhite);
@@ -78,5 +63,4 @@ namespace MohawkGame2D
             Draw.Circle(player2x, player2y, 25);
         }
     }
-
 }
