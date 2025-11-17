f3064c63586de055181cb08fbb47598b3441c277
grace-h2004 <gracehundertmark@yaho.ca>
2025-11-17 13:54:23 -0500

    player assets loaded

--- Assignment 4-Group4/Assignment 4-Group4/Game.cs
+++ Assignment 4-Group4/Assignment 4-Group4/Game.cs
@@ -12,25 +12,29 @@ namespace MohawkGame2D
     public class Game
     {
         // Place your variables here:
-        float player1x = 0.0f;
-        float player1y = 0.0f;
-        float player2x = 0.0f;
-        float player2y = 0.0f;
+        float player1x = 100.0f;
+        float player1y = 400.0f;
+        float player2x = 850.0f;
+        float player2y = 400.0f;
         float player1Speed = 4.0f;
         float player2Speed = 4.0f;
 
         //variables for graphics
-
+        Texture2D sonic;
+        Texture2D tails;
 
         /// <summary>
         ///     Setup runs once before the game loop begins.
         /// </summary>
         public void Setup()
         {
+            //general config
             Window.SetTitle("Assignment 4 - Group 4");
             Window.SetSize(1200, 600);
 
             //load graphics
+            sonic = Graphics.LoadTexture("../../../../../assets/graphics/sonic.png");
+            tails = Graphics.LoadTexture("../../../../../assets/graphics/tails.png");
         }
 
         /// <summary>
@@ -40,6 +44,7 @@ namespace MohawkGame2D
         {
             Window.ClearBackground(Color.OffWhite);
 
+            //player movement
             if (Raylib.IsKeyDown(KeyboardKey.W))
             {
                 player1y -= player1Speed;
@@ -72,11 +77,12 @@ namespace MohawkGame2D
             {
                 player2x += player2Speed;
             }
-            Draw.FillColor = (Color.Red);
-            Draw.Circle(player1x, player1y, 25);
+
+            //draw sonic asset
+            Graphics.Draw(sonic, player1x, player1y);
            
-            Draw.FillColor = (Color.Green);
-            Draw.Circle(player2x, player2y, 25);
+           //draw tails asset
+            Graphics.Draw(tails, player2x, player2y);
         }
     }
 
