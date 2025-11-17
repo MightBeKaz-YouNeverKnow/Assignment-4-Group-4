f90ed4161a6df715a8d15ae092bf5998fa546d36
JamesonPhong <000882371@mohawkcollege.ca>
2025-11-17 13:38:00 -0500

    Add Collision Detection in PlayerCollisionDetection function

--- Assignment 4-Group4/Assignment 4-Group4/Game.cs
+++ Assignment 4-Group4/Assignment 4-Group4/Game.cs
@@ -79,7 +79,6 @@ namespace MohawkGame2D
             {
                 playerOneLeftCollidePlayerTwoRight = false;
             }
-
             // Check if Left side of Player 1 collides with Right side of Player 2
             if (player1x - 25 == player2x + 25)
             {
@@ -89,7 +88,6 @@ namespace MohawkGame2D
             {
                 playerOneRightCollidePlayerTwoLeft = false;
             }
-
             // Check if Bottom of Player 1 collides with Top of Player 2
             if (player1y + 25 == player2y - 25)
             {
@@ -99,7 +97,6 @@ namespace MohawkGame2D
             {
                 playerOneBottomCollidePlayerTwoTop = false;
             }
-
             // Check if Top of Player 1 collides with Bottom of Player 2
             if (player1y - 25 == player2y + 25)
             {
@@ -108,6 +105,35 @@ namespace MohawkGame2D
             else
             {
                 playerOneTopCollidePlayerTwoBottom = false;
+            }
+
+            /* 
+                SEPERATOR
+            */
+
+            // Bounce Players away from each other (P1 = Left | P2 = Right)
+            if (playerOneLeftCollidePlayerTwoRight == true)
+            {
+                player1x -= player1Speed * 2;
+                player2x += player2Speed * 2;
+            }
+            // Bounce Players away from each other (P1 = Right | P2 = Left)
+            if (playerOneRightCollidePlayerTwoLeft == true)
+            {
+                player1x += player1Speed * 2;
+                player2x -= player1Speed * 2;
+            }
+            // Bounce Players away from each other (P1 = Bottom | P2 = Top)
+            if (playerOneBottomCollidePlayerTwoTop == true)
+            {
+                player1y -= player1Speed * 2;
+                player2y += player2Speed * 2;
+            }
+            // Bounce Players away from each other (P1 = Top | P2 = Bottom)
+            if (playerOneTopCollidePlayerTwoBottom == true)
+            {
+                player1y += player1Speed * 2;
+                player2y -= player1Speed * 2;
             }
         }
     }
