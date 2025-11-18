39d899df3bbaa3d30573317a67a4479469c731d1
JamesonPhong <000882371@mohawkcollege.ca>
2025-11-18 01:15:36 -0500

    Add Setup function in PlayerTwo class

--- Assignment 4-Group4/Assignment 4-Group4/PlayerTwo.cs
+++ Assignment 4-Group4/Assignment 4-Group4/PlayerTwo.cs
@@ -5,5 +5,15 @@ namespace MohawkGame2D
 {
     public class PlayerTwo
     {
+        public Vector2 position = new Vector2(1100, 500);
+        public Vector2 velocity;
+        public void Setup()
+        {
+            // Draw Player 2
+            Draw.LineSize = 1;
+            Draw.LineColor = Color.Black;
+            Draw.FillColor = Color.Red;
+            Draw.Circle(position.X, position.Y, 25);
+        }
     }
 }
