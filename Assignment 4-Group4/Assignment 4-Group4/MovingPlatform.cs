52ef464d0cf24386706dab0e800d765b3564e25d
JamesonPhong <000882371@mohawkcollege.ca>
2025-11-26 18:38:20 -0500

    Add code content into MovingPlatform class

--- Assignment 4-Group4/Assignment 4-Group4/MovingPlatform.cs
+++ Assignment 4-Group4/Assignment 4-Group4/MovingPlatform.cs
@@ -9,21 +9,31 @@ namespace MohawkGame2D
         Vector2 velocity;
         public int width;
         public int height;
-        public void Setup()
+        Color color;
+        public void Setup(int x, int y, int speed, int w)
         {
             // Spawn position of platforms
+            position = new Vector2(x, y);
 
-            // Direction that platform moves towards
+            // Random direction that platform moves towards
+            Vector2 direction = new Vector2(Random.Integer(-1, 1), Random.Integer(-1, 1));
+            velocity = direction * speed;
 
             // Platform size & colour
-
+            width = w;
+            height = 5;
+            color = Color.Black;
         }
         public void Update()
         {
             // Movement of the platforms
+            position += velocity * Time.DeltaTime;
 
             // Draw the platforms
-
+            Draw.LineSize = 1;
+            Draw.LineColor = color;
+            Draw.FillColor = color;
+            Draw.Rectangle(position.X, position.Y, width, height);
         }
     }
 }
