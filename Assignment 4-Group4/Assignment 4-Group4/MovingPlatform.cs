c2b342a1bb1dbd2eccab1a012048aefc5e0c2aef
JamesonPhong <000882371@mohawkcollege.ca>
2025-11-26 18:28:25 -0500

    Add code comments to determine what to do

--- Assignment 4-Group4/Assignment 4-Group4/MovingPlatform.cs
+++ Assignment 4-Group4/Assignment 4-Group4/MovingPlatform.cs
@@ -5,12 +5,24 @@ namespace MohawkGame2D
 {
     public class MovingPlatform
     {
+        public Vector2 position;
+        Vector2 velocity;
+        public int width;
+        public int height;
         public void Setup()
         {
+            // Spawn position of platforms
+
+            // Direction that platform moves towards
+
+            // Platform size & colour
 
         }
         public void Update()
         {
+            // Movement of the platforms
+
+            // Draw the platforms
 
         }
     }
