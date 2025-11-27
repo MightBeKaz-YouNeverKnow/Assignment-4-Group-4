2790befeab77d756708569bde51c98124b0d35fb
MightBeKaz-YouNeverKnow <000972147@mohawkcollege.ca>
2025-11-27 14:02:20 -0500

    testing no updates

--- Assignment 4-Group4/Assignment 4-Group4/obj/Debug/net9.0/Assignment 4-Group4.AssemblyInfo.cs
+++ Assignment 4-Group4/Assignment 4-Group4/obj/Debug/net9.0/Assignment 4-Group4.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assignment 4-Group4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b92fffd2640ee0f73feb2a927e32c296971c448e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a86d61223141d4f750a05070c94e7eeae88d6f5b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assignment 4-Group4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assignment 4-Group4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
